bbfacb4242eb8fc02e3ae1bd9852533ff77ef3ee
Troy Tingle <troy.tingle1@gmail.com>
2025-02-10 18:44:47 +0000

    Added signup page and API call

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,19 +1,55 @@
 import axios from 'axios';
+import { User } from '../model/models';
 
 const API_URL = 'https://your-api-server.com'; // Replace with your API server URL
 
-interface LoginResponse {
+interface AuthResponse {
   status: number;
   data?: string;
   message?: string;
 }
 
 export const login = async (
-  email: string,
-  password: string
-): Promise<LoginResponse> => {
+  email: User['email'],
+  password: User['password']
+): Promise<AuthResponse> => {
   try {
     const response = await axios.post(`${API_URL}/api/auth/login`, {
+      email: email,
+      password: password,
+    });
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        return {
+          status: error.response.status,
+          message: error.response.data.message || 'An error occurred',
+        };
+      }
+      return {
+        status: 500,
+        message: 'Server error',
+      };
+    }
+    return {
+      status: 500,
+      message: 'An unexpected error occurred',
+    };
+  }
+};
+
+export const signup = async (
+  username: User['username'],
+  email: User['email'],
+  password: User['password']
+): Promise<AuthResponse> => {
+  try {
+    const response = await axios.post(`${API_URL}/api/auth/signup`, {
+      username: username,
       email: email,
       password: password,
     });
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,102 @@
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+
+const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!email || !password) {
+      setError('Email and password are required');
+    } else {
+      setError('');
+      // TODO: call api and redirect to signin
+    }
+  };
+
+  return (
+    <Container
+      component='main'
+      maxWidth='xs'
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        minWidth: '100vw',
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant='h5'>Signup</Typography>
+        {error && (
+          <Typography color='error' sx={{ mt: 2 }}>
+            {error}
+          </Typography>
+        )}
+        <Box component='form' onSubmit={handleSubmit} sx={{ mt: 2 }}>
+          <TextField
+            fullWidth
+            label='Username'
+            margin='normal'
+            type='input'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            error={!!error}
+          />
+          <TextField
+            fullWidth
+            label='Email Address'
+            margin='normal'
+            type='input'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            error={!!error}
+          />
+          <TextField
+            fullWidth
+            label='Password'
+            margin='normal'
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            error={!!error}
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Signup
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Signup;
