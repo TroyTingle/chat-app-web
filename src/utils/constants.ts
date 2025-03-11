b828732150ad2c242e482af732c106d3ee77da08
Troy Tingle <troy.tingle1@gmail.com>
2025-03-11 18:04:00 +0000

    Updating docker ignore and constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
-export const SOCKET_URL = import.meta.env.VITE_API_WEBSOCKET_URL || "ws://localhost:8080/ws";
+export const BASE_API_URL = import.meta.env.VITE_API_URL;
+export const SOCKET_URL = import.meta.env.VITE_API_WEBSOCKET_URL;
