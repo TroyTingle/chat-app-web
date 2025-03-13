2b90fe422116b65af5c75644ebfe9fea8b9bad07
Troy Tingle <troy.tingle1@gmail.com>
2025-03-13 20:18:47 +0000

    Adding base code for new framework

--- src/api/messages.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "../config/axiosConfig";
-import { Chat, Message } from "../model/models";
-
-export const getMessagesForChatId = async (chatId: Chat["id"]): Promise<Message[]> => {
-  const response = await api.get<Message[]>(`/api/messages/${chatId}`);
-  return response.data;
-};
--- src/utils/constants.ts
+++
@@ -1,2 +0,0 @@
-export const BASE_API_URL = import.meta.env.VITE_API_URL;
-export const SOCKET_URL = import.meta.env.VITE_API_WEBSOCKET_URL;
